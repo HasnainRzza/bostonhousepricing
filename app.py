4e9e9d50a4e84403943e411e6700d3204c3d8383
HasnainRaza <workflow.raza@gmail.com>
2025-06-05 23:51:06 +0500

    changes

--- app.py
+++ app.py
@@ -24,9 +24,16 @@ def predict_api():
     new_data = scaler.transform(np.array((list(data.values()))).reshape(1,-1))
     
     output = model.predict(new_data)
-    print(output)
+    print("iam output",output)
     return jsonify(output[0])
 
+@app.route("/predict", methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = model.predict(final_input)[0]
+    return render_template("home.html", prediction_text = "this house is priced at {}".format(output))
 
 if __name__ == '__main__':
     app.run(debug=True)
